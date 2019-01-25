748fc76f64023d6a76ef2106031cab0de2387ef0
Airren <renqiqiang@outlook.com>
2019-01-25 11:49:09 +0800

    DTLZ 3 6 最优解增加

--- examples/paper_experiment_1_100.py
+++ examples/paper_experiment_1_100.py
@@ -10,11 +10,12 @@ import multiprocessing as mp
 from platypus import  *
 
 # from examples import paper_experiment
-import paper_experiment
+# import paper_experiment
+import paper_experiment_multi
 
 # problems= [ZDT1,ZDT2,ZDT3,ZDT4,ZDT6]
-problems= [WFG1,WFG2,WFG3,WFG4,WFG5,WFG6,WFG7,WFG8,WFG9]
-# problems = [DTLZ1,DTLZ2,DTLZ3,DTLZ4,DTLZ7]
+# problems= [WFG1,WFG2,WFG3,WFG4,WFG5,WFG6,WFG7,WFG8,WFG9]
+problems = [DTLZ1,DTLZ2,DTLZ3,DTLZ4,DTLZ7]
 with open('../results/igd.csv', 'w+') as F:
     F.write(',RL_NSGAII,NSGAII,RL_NSGAIII,NSGAIII\n')
 
@@ -24,7 +25,7 @@ for problem in problems:
 
         processes = []
         for i in range(38):
-            processes.append(mp.Process(target= paper_experiment.compare_experiment,args=(problem(2),)))
+            processes.append(mp.Process(target= paper_experiment.compare_experiment,args=(problem(3),)))
         for i in range(38):
             processes[i].start()
         for i in range(38):
